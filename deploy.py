1c367e0288f6130a154a837e74852eaf3bbec557
DevOpsEtc <DevOpsEtc@users.noreply.github.com>
2018-03-06 15:34:14 -0700

    refactor: moved back to single deployment stack; updates cfn template resource properties for CDN distro

--- deploy.py
+++ deploy.py
@@ -9,11 +9,11 @@ def main():
     """Create/Update/Delete CloudFormation stack to deploy S3 static website.
 
     Launches a CloudFormation stack that creates:
+        - One ACM SSL/TLS certificate
         - One IAM group
         - One IAM group policy
         - One IAM user
         - Two S3 buckets
-        - One ACM SSL/TLS certificate
         - One CloudFront distribution resource
 
     Also, wrapper script checks for existing stack and if found, prompts to
@@ -21,20 +21,18 @@ def main():
     via a delete.
     """
 
-    region_cert = 'us-east-1' # overide local AWS config; needed for certificate
+    region = 'us-east-1' # overide any local AWS config; needed for ACM cert
     profile = 'Static-Site'
     cf_stack = profile + '-Stack'
-    cf_stack_cert = profile + '-Cert-Stack'
-    cf_temp = 'cfn/site.cfn.yaml'
-    cf_temp_cert = 'cfn/cert.cfn.yaml'
+    cf_temp = 'site.cfn.yaml'
     domain = 'devopsetc.com'
     params =  [
         {"ParameterKey": "DomainName","ParameterValue": domain},
-        {"ParameterKey": "GroupName","ParameterValue": profile}
+        {"ParameterKey": "GroupName","ParameterValue": profile},
+        {"ParameterKey": "Region","ParameterValue": region}
     ]
     account = boto3.client('sts').get_caller_identity()['Account']
-    cf = boto3.client('cloudformation')
-    cf_cert = boto3.client('cloudformation', region_name=region_cert)
+    cf = boto3.client('cloudformation', region_name=region)
 
     init(autoreset=True) # colorama: automatically reset style after each call
 
@@ -51,8 +49,7 @@ def main():
         cf.describe_stacks(StackName=cf_stack)
     except ClientError as e:
         if e.response['Error']['Message'].endswith('does not exist'):
-            launch_stack(cf, cf_cert, cf_temp, cf_temp_cert, cf_stack,
-                cf_stack_cert, params)
+            launch_stack(cf, cf_temp, cf_stack, params)
     else:
         print(Fore.RED + '\nStack already exists:', cf_stack)
         prompt = Fore.YELLOW + '\nUpdate|Delete|Cancel (U|D|C): ' + Fore.RESET
@@ -63,55 +60,23 @@ def main():
                 return True
             if reply[:1] == 'd':
                 delete_stack(cf, cf_stack, domain)
-                delete_stack_cert(cf_cert, cf_stack_cert)
                 return False
             if reply[:1] == 'c':
                 return False
 
-def launch_stack(cf, cf_cert, cf_temp, cf_temp_cert, cf_stack, cf_stack_cert,
-    params):
-    print(Fore.CYAN + '\nAn ACM SSL/TSL certificate will be generated and '
+def launch_stack(cf, cf_temp, cf_stack, params):
+    print('\nAn ACM SSL/TSL certificate will be generated and '
         'validation email sent to:\n'
         '\n- WHOIS listed domain registrant & technical/admin contacts'
         '\n- Administrator, hostmaster, postmaster, webmaster and admin'
         '@your_domain_name\n'
-        '\nYou will need to click the approval link on one of them for '
-        'deployment to finish'
+        '\nYou will need to click the approval link in ONE of them for '
+        'deployment to finish.'
     )
 
     wait = input(Fore.YELLOW + '\nPress enter to continue...' + Fore.RESET)
 
-    try:
-        cf_cert.describe_stacks(StackName=cf_stack_cert)
-    except ClientError as e:
-        if e.response['Error']['Message'].endswith('does not exist'):
-            pass
-    else:
-        print(Fore.RED + '\nFound Existing Stack: ' + cf_stack_cert)
-        delete_stack_cert(cf_cert, cf_stack_cert)
-
     print('')
-    spin_start = '\nLaunching Stack: '+ cf_stack_cert
-    spinner = Halo(text=spin_start, spinner='circleHalves')
-    spinner.start()
-
-    with open(cf_temp_cert, 'r') as f:
-        cfn_temp_cert = f.read()
-
-    cf_cert.create_stack(
-        StackName=cf_stack_cert,
-        TemplateBody=cfn_temp_cert,
-        Parameters=params,
-        TimeoutInMinutes=30,
-        OnFailure='ROLLBACK'
-    )
-
-    waiter = cf_cert.get_waiter('stack_create_complete')
-    waiter.wait(StackName=cf_stack_cert)
-
-    spin_success = '\nStack Created: '+ cf_stack_cert
-    spinner.succeed(text=spin_success)
-
     spin_start = '\n\nLaunching Stack: '+ cf_stack
     spinner = Halo(text=spin_start, spinner='circleHalves')
     spinner.start()
@@ -123,7 +88,7 @@ def launch_stack(cf, cf_cert, cf_temp, cf_temp_cert, cf_stack, cf_stack_cert,
         StackName=cf_stack,
         TemplateBody=cfn_temp,
         Parameters=params,
-        TimeoutInMinutes=10,
+        TimeoutInMinutes=15,
         Capabilities=['CAPABILITY_NAMED_IAM'],
         OnFailure='ROLLBACK'
     )
@@ -169,11 +134,25 @@ def delete_stack(cf, cf_stack, domain):
     spinner.start()
 
     # Bucket can't be deleted unless empty
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(domain)
-    bucket_log = s3.Bucket(domain + '-log')
-    bucket.objects.all().delete()
-    bucket_log.objects.all().delete()
+    s3 = boto3.resource('s3', region_name=region)
+
+    try:
+        s3.meta.client.head_bucket(Bucket=domain)
+    except ClientError as e:
+        if e.response['Error']['Message'].endswith('does not exist'):
+            pass
+    else:
+        bucket = s3.Bucket(domain)
+        bucket.objects.all().delete()
+
+    try:
+        s3.meta.client.head_bucket(Bucket=domain + '-log')
+    except ClientError as e:
+        if e.response['Error']['Message'].endswith('does not exist'):
+            pass
+    else:
+        bucket_log = s3.Bucket(domain + '-log')
+        bucket_log.objects.all().delete()
 
     cf.delete_stack(StackName=cf_stack)
 
@@ -181,20 +160,6 @@ def delete_stack(cf, cf_stack, domain):
     waiter.wait(StackName=cf_stack)
 
     spin_success = '\nStack Deleted: '+ cf_stack
-    spinner.succeed(text=spin_success)
-
-def delete_stack_cert(cf_cert, cf_stack_cert):
-    print('')
-    spin_start = '\nDeleting Stack: '+ cf_stack_cert
-    spinner = Halo(text=spin_start, spinner='circleHalves')
-    spinner.start()
-
-    cf_cert.delete_stack(StackName=cf_stack_cert)
-
-    waiter = cf_cert.get_waiter('stack_delete_complete')
-    waiter.wait(StackName=cf_stack_cert)
-
-    spin_success = '\nStack Deleted: '+ cf_stack_cert
     spinner.succeed(text=spin_success)
 
 if __name__ == '__main__':
